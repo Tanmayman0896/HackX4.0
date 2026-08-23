081f6b583e2540d103413d7193428cebb9baef24
Tanmoy mandal <tanmaymandal1250@gmail.com>
2026-08-23 13:18:47 +0530

    slug added: whatsapp and payement link

---
+++ src/app/payment-link/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function RegisterPage() {
+  redirect("https://formbuilder.ccavenue.com/live/ccavenue/manipal-university-jaipur-2/muj-hackx-40-2026010");
+}
---
+++ src/app/whatsapp-group/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function RegisterPage() {
+  redirect("https://chat.whatsapp.com/BJc865yfzD8K5wm0eFfFxa");
+}
