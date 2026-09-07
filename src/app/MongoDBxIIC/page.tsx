6da94f7731b508ab05e416a09d6ac6075b560d5b
Ronit Mahajan <ronitmahajann270@gmail.com>
2026-09-08 00:22:46 +0530

    mongodbxiic

--- src/app/MongoDBxIIC/page.tsx
+++ src/app/MongoDBxIIC/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
-export default function MongoDBPage() {
+export default function MongoDBxIICPage() {
   redirect("https://mongodb.zoom.com/j/93664049219?pwd=bqiKFaHqvwUmszk1UYIogu5cetwQ9a.1");
 }
